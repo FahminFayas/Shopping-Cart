59e21e85219dc1522d5eb6d48801ddbfc40f9d81
Fahmin <akfahmin@gmail.com>
2024-05-20 23:10:51 +0530

    chore: Update product-helper.js to use correct collection name

--- config/helpers/product-helper.js
+++ config/helpers/product-helper.js
@@ -1,5 +1,5 @@
 var db = require('../connections');
-var collection = require('../connections')
+var collection = require('../collections')
 module.exports = {
     addProduct: (product, callback) => {
         db.get().collection('product').insertOne(product).then((data) => {
@@ -9,7 +9,7 @@ module.exports = {
     },
     getAllProducts: () => {
         return new Promise(async (resolve, reject) => {
-            let products = await db.get().collection('product').find().toArray();
+            let products = await db.get().collection(collection.PRODUCT_COLLECTIONS).find().toArray();
             resolve(products);
         });
     }
