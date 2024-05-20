e2e179d36c165fe3f25be8d07045d20293d03da4
Fahmin <akfahmin@gmail.com>
2024-05-20 23:06:00 +0530

    feat: Create collections.js and import in product-helper.js

--- config/helpers/product-helper.js
+++ config/helpers/product-helper.js
@@ -1,4 +1,5 @@
 var db = require('../connections');
+var collection = require('../connections')
 module.exports = {
     addProduct: (product, callback) => {
         db.get().collection('product').insertOne(product).then((data) => {
