0faa3770fb1f27ef753f6db59e379c8240e0944d
Fahmin <akfahmin@gmail.com>
2024-05-20 17:11:19 +0530

    Created connections.js for MongoDB connection. Installed mongodb package.

---
+++ config/connections.js
@@ -0,0 +1,15 @@
+const mongoClient = require('mongodb').mongoClient;
+const state ={
+    db: null
+}
+module.exports.connect = function(done){
+    const url = 'mongodb://localhost:27017';
+    const dbName = 'shoppingDB';
+    mongoClient.connect(url, function(err, data){
+        if(err){
+            return done(err);
+        }
+        state.db = data.db(dbName);
+        done();
+    });
+}
