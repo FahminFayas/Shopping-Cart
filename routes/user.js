1815533d90d36faeefa25bf03f8909f445331461
Fahmin <akfahmin@gmail.com>
2024-05-22 00:20:02 +0530

    refactor: Update user.js to handle user login status and redirection

--- routes/user.js
+++ routes/user.js
@@ -5,31 +5,38 @@ const userHelper = require('../config/helpers/user-helpers');
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   let user = req.session.user;
   console.log(user);
-  productHelper.getAllProducts().then((products)=>{
-  res.render('user/view-products',{products,user})
+  productHelper.getAllProducts().then((products) => {
+    res.render('user/view-products', { products, user })
 
   })
 });
-router.get('/login',(req, res) => {
+router.get('/login', (req, res) => {
   res.render('user/login');
 });
-router.get('/signup',(req, res) => {
+router.get('/signup', (req, res) => {
   res.render('user/signup');
 });
-router.post('/signup',(req, res) => {
-  userHelper.doSignup(req.body).then((response)=>{
+router.post('/signup', (req, res) => {
+  userHelper.doSignup(req.body).then((response) => {
     console.log(response);
   })
 });
-router.post('/login',(req, res) => {
-  userHelper.doLogin(req.body).then((response)=>{
-    console.log(response);
-  })
+router.post('/login', (req, res) => {
+  userHelper.doLogin(req.body).then((response) => {
+    if (response.status) {
+      req.session.loggedIn = true;
+      req.session.user = response.user;
+      res.redirect('/');
+    } else {
+      res.redirect('/login');
+
+    }
+  });
 });
-router.get('/logout',(req, res) => {
+router.get('/logout', (req, res) => {
   req.session.destroy();
   res.redirect('/');
 });
