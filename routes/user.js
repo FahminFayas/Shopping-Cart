a1b2000cb7e9436dc5810e6027409dffaa937dc6
Fahmin <akfahmin@gmail.com>
2024-05-21 20:05:45 +0530

    feat: Add login and signup routes in users.js

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var productHelper = require('../config/helpers/product-helper');
+const userHelper = require('../config/helpers/user-helpers');
 
 
 /* GET home page. */
@@ -9,6 +10,17 @@ router.get('/', function(req, res, next) {
     console.log(products);
   res.render('user/view-products',{admin: false,products})
 
+  })
+});
+router.get('/login',(req, res) => {
+  res.render('user/login');
+});
+router.get('/signup',(req, res) => {
+  res.render('user/signup');
+});
+router.post('/signup',(req, res) => {
+  userHelper.doSignup(req.body).then((response)=>{
+    console.log(response);
   })
 });
 
