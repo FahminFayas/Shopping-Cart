e2d16c80c7066d450b218a905732609f2b7f499e
Fahmin <akfahmin@gmail.com>
2024-05-21 23:33:40 +0530

    feat: Add session management with express-session in app.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var hbs = require('express-handlebars');
 var app = express();
 var fileUpload = require('express-fileupload');
 var db = require('./config/connections');
+var session = require('express-session');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
+app.use(session({secret:"Key",cookie:{maxAge:600000}}));
 
 db.connect((err) => {
   if(err){
