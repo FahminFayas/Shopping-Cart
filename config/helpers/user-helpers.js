d89130cc83ce6dcc7fdc98ff07f7bccdf7460d64
Fahmin <akfahmin@gmail.com>
2024-05-21 23:24:40 +0530

    feat: Add doLogin function for user authentication in user-helpers.js

--- config/helpers/user-helpers.js
+++ config/helpers/user-helpers.js
@@ -16,7 +16,26 @@ module.exports = {
                 console.log(userData._id);
                 resolve(data.insertedId);
             });
-        }
-        )
+        });
+    },
+    doLogin:(userData)=>{
+        return new Promise(async(resolve,reject)=>{
+            let loginStatus = false;
+            let response = {};
+            let user = await db.get().collection(collection.USER_COLLECTION).findOne({
+                Email:userData.Email
+            });
+            if(user){
+                bcrypt.compare(userData.password,user.password).then((status)=>{
+                    if(status){
+                        console.log('Login success');
+                    }else{
+                        console.log('Login failed');
+                    }
+                });
+            }else{
+                console.log('Login failed');
+            }
+        })
     }
 }
