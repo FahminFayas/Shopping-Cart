2bcf5b9245c7232e9b9db61f8e760c1a571a12ad
Fahmin <akfahmin@gmail.com>
2024-05-22 16:20:37 +0530

    feat: Add deleteProduct function in product-helper.js

--- config/helpers/product-helper.js
+++ config/helpers/product-helper.js
@@ -1,5 +1,6 @@
 var db = require('../connections');
-var collection = require('../collections')
+var collection = require('../collections');
+var objectId = require('mongodb').ObjectId;
 module.exports = {
     addProduct: (product, callback) => {
         db.get().collection('product').insertOne(product).then((data) => {
@@ -11,6 +12,13 @@ module.exports = {
         return new Promise(async (resolve, reject) => {
             let products = await db.get().collection(collection.PRODUCT_COLLECTIONS).find().toArray();
             resolve(products);
+        });
+    },
+    deleteProduct: (proId) => {
+        return new Promise((resolve, reject) => {
+            db.get().collection(collection.PRODUCT_COLLECTIONS).deleteOne({ _id: new objectId(proId)}).then((response) => {
+                resolve(response);
+            })
         });
     }
 }
