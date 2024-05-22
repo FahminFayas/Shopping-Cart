fb9772a034bc4390e07cfcecbc7b977bfff78c43
Fahmin <akfahmin@gmail.com>
2024-05-22 13:53:38 +0530

    fix: Pass login error message to login view and reset after rendering in user.js

--- routes/user.js
+++ routes/user.js
@@ -17,7 +17,8 @@ router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
   } else {
-    res.render('user/login');
+    res.render('user/login',{loginErr:req.session.loginErr});
+    req.session.loginErr = false;
   }
 });
 router.get('/signup', (req, res) => {
@@ -35,6 +36,7 @@ router.post('/login', (req, res) => {
       req.session.user = response.user;
       res.redirect('/');
     } else {
+      req.session.loginErr = "Invalid username or password";
       res.redirect('/login');
 
     }
