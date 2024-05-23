b5f9b2c5aa2e3a1d7e60d38c5b22cc8039cedc2f
Fahmin <akfahmin@gmail.com>
2024-05-23 19:37:17 +0530

    feat: Add getProductDetails and updateProduct functions in product-helpers.js

--- config/helpers/product-helper.js
+++ config/helpers/product-helper.js
@@ -20,5 +20,26 @@ module.exports = {
                 resolve(response);
             })
         });
+    },
+    getProductDetails: (proId) => {
+        return new Promise((resolve, reject) => {
+            db.get().collection(collection.PRODUCT_COLLECTIONS).findOne({ _id: new objectId(proId) }).then((product) => {
+                resolve(product);
+            });
+        });
+    },
+    updateProduct: (proId, proDetails) => {
+        return new Promise((resolve, reject) => {
+            db.get().collection(collection.PRODUCT_COLLECTIONS).updateOne({ _id: new objectId(proId) }, {
+                $set: {
+                    Name: proDetails.Name,
+                    Description: proDetails.Description,
+                    Price: proDetails.Price,
+                    Category: proDetails.Category
+                }
+            }).then((response) => {
+                resolve();
+            });
+        });
     }
 }
